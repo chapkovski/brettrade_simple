45096ec0b0d69d35960d5bf40349503a84ac0036
Francisco Kahil <codexico@gmail.com>
2021-10-28 19:00:23 +0300

    updotree

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/crossmerged/" : "/",
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/brettrade_simple/" : "/",
   lintOnSave: false,
   transpileDependencies: ["vuetify"],
 };
